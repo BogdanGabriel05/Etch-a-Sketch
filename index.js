799bc3db4c8d1dc2107fa01975a4570c9242c19f
BogdanGabriel05 <bogdan0511@Xubuntu.myguest.virtualbox.org>
2024-11-02 13:16:11 +0200

    created the 16x16 divs
    I created the initial divs using a for loop

--- index.js
+++ index.js
@@ -3,10 +3,34 @@
 // Big Container location
 const container = document.querySelector(".container");
 
-//Append newDiv
-const newDiv = document.createElement("div");
-newDiv.classList.add("newDiv");
-container.appendChild(newDiv);
+//Create 16x16 divs
+for(let i = 1; i <= 256; i++) {
+    const newDivs = document.createElement("div");
+    newDivs.classList.add("newDivs");
+    container.appendChild(newDivs);
+}
+
+//button 
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", userNumber);
+function userNumber() {
+    prompt("Enter number!")
+}
+
+// // //user input > 16
+// if(prompt > 16) {
+//     for(let i = 16; i <= prompt; i++) {
+//         divCreation();
+//     }
+// }
+
+// // div creation function
+// function divCreation() {
+//     const newDivs100 = document.createElement("div");
+//     newDivs100.classList.add("newDivs100");
+//     container.appendChild(newDivs100);
+// }
+
 
 
 
