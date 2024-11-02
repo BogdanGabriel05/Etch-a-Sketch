87555770eface00dee96e652bdf08cf9cec42970
BogdanGabriel05 <bogdan0511@Xubuntu.myguest.virtualbox.org>
2024-11-02 11:34:51 +0200

    created the measures
    redifines the measures for the original first div

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // DIV CREATION
 
+// Big Container location
+const container = document.querySelector(".container");
+
+//Append newDiv
+const newDiv = document.createElement("div");
+newDiv.classList.add("newDiv");
+container.appendChild(newDiv);
+
+
+
+
+
 
