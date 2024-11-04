0120c3428cda7f1e205abee2b08ca9b22f5e99de
BogdanGabriel05 <bogdan0511@Xubuntu.myguest.virtualbox.org>
2024-11-04 12:44:41 +0200

    Created the hover effect
    Used a random generator formula for color and applied it to the divs.

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ for(let i = 1; i <= 256; i++) {
     const newDivs = document.createElement("div");
     newDivs.classList.add("newDivs");
     container.appendChild(newDivs);
+
+//random color
+function bgColor() {
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+newDivs.style.backgroundColor = "#" + randomColor;
 }
+
+ //Mouseenter Event
+newDivs.addEventListener("mouseenter", bgColor)
+// //Mouseleave Event
+newDivs.addEventListener("mouseleave", bgColor)
+
+}
+    
+
+
+
+
+
+
+
+
+
 
 //button 
 const btn = document.querySelector(".btn");
@@ -16,20 +38,6 @@ btn.addEventListener("click", userNumber);
 function userNumber() {
     prompt("Enter number!")
 }
-
-// // //user input > 16
-// if(prompt > 16) {
-//     for(let i = 16; i <= prompt; i++) {
-//         divCreation();
-//     }
-// }
-
-// // div creation function
-// function divCreation() {
-//     const newDivs100 = document.createElement("div");
-//     newDivs100.classList.add("newDivs100");
-//     container.appendChild(newDivs100);
-// }
 
 
 
